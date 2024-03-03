01c71bad8b7eb4327b83816df5bfb1910f2f49c0
Sachu S Kumar <sachusajith170@gmail.com>
2024-03-03 21:43:59 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,9 @@
+
+document.getElementById('mobile-menu-toggle').addEventListener('click', function () {
+    var mobileMenu = document.getElementById('mobile-menu');
+    if (mobileMenu.classList.contains('hidden')) {
+        mobileMenu.classList.remove('hidden');
+    } else {
+        mobileMenu.classList.add('hidden');
+    }
+});
